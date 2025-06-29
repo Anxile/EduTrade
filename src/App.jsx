f03ca4d10f39d66e046e03e884922659ca0631dc
Anxile <onebox.chen@outlook.com>
2025-06-28 23:40:57 -0400

    hello world

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import { Button } from '@/components/ui/button'
 import './App.css'
+import Navbar from './page/Navbar/Navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <Button>Yihe</Button>
+      <Navbar/>
     </>
   )
 }
